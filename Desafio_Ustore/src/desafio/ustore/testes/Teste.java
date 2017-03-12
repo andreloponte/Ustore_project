0624cdd8ae29c3110bf971eb99a7e23b2c87db8e
andreloponte <andreloponte@gmail.com>
2017-03-12 00:48:49 -0300

    Trabalho refeito e problema resolvido

---
+++ Desafio_Ustore/src/desafio/ustore/testes/Teste.java
@@ -0,0 +1,20 @@
+package desafio.ustore.testes;
+
+import desafio.ustore.objetos.Usuario;
+import desafio.ustore.repositorio.RepositorioUsuario;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		RepositorioUsuario repositorioUsuario = new RepositorioUsuario();
+//		Usuario usuario = new Usuario();
+//		usuario.setNome("Penélope");
+//		usuario.setSenha("museologia");
+//		usuario.setEmail("penelopebosio@yahoo.com");
+//		usuario.setTelefone(995368993);
+		
+//		repositorioUsuario.salvar(usuario);
+	}
+
+}
